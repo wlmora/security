688ed836ac3ffb82cca35aa4e46322da80db2189
William Londoño <wlmora@gmail.com>
2023-07-28 12:38:28 -0500

    Integration with Keycloak

---
+++ src/main/java/com/business/security/config/JwtAuthenticationConverter.java
@@ -0,0 +1,64 @@
+package com.business.security.config;
+
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.jwt.JwtClaimNames;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
+import org.springframework.stereotype.Component;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationConverter implements Converter<Jwt, AbstractAuthenticationToken> {
+
+    private static final JwtGrantedAuthoritiesConverter jwtGrantedAuthoritiesConverter =
+            new JwtGrantedAuthoritiesConverter();
+
+    @Value("${jwt.auth.converter.resource-id}")
+    private String resourceId;
+
+    @Value("${jwt.auth.converter.principle-attribute}")
+    private String principleAttribute;
+
+    @Override
+    public AbstractAuthenticationToken convert(@NonNull Jwt jwt) {
+        Collection<GrantedAuthority> authorities = Stream
+                .concat(jwtGrantedAuthoritiesConverter.convert(jwt).stream(), extractResourceRoles(jwt).stream())
+                .toList();
+        return new JwtAuthenticationToken(jwt, authorities, getPrincipleClaimName(jwt));
+    }
+
+    private String getPrincipleClaimName(Jwt jwt) {
+        String claimName = JwtClaimNames.SUB;
+        if (this.principleAttribute != null) {
+            claimName = this.principleAttribute;
+        }
+        return jwt.getClaim(claimName);
+    }
+
+    private Collection<? extends GrantedAuthority> extractResourceRoles(Jwt jwt) {
+        Map<String, Object> resourceAccess = jwt.getClaim("resource_access");
+        Map<String, Object> resource;
+        Collection<String> resourceRoles;
+        if (resourceAccess == null
+                || (resource = (Map<String, Object>) resourceAccess.get(this.resourceId)) == null
+                || (resourceRoles = (Collection<String>) resource.get("roles")) == null) {
+            return List.of();
+        }
+        return resourceRoles.stream()
+                .map(role -> new SimpleGrantedAuthority("ROLE_".concat(role)))
+                .collect(Collectors.toList());
+    }
+}
---
+++ src/main/java/com/business/security/user/TestController.java
@@ -0,0 +1,21 @@
+package com.business.security.user;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/hello-1")
+    @PreAuthorize("hasRole('admin_client_role')")
+    public String helloAdmin(){
+        return "Hello Sprig Boot With Keycloak with ADMIN";
+    }
+
+    @GetMapping("/hello-2")
+    @PreAuthorize("hasRole('user_client_role') or hasRole('admin_client_role')")
+    public String helloUser(){
+        return "Hello Sprig Boot With Keycloak with USER";
+    }
+}
